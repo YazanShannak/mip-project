6ac93d512642cfd914321dbf66492531954b2f2c
yazan <yazansh7@hotmail.com>
2020-11-30 22:45:20 +0200

    Update data download link

--- download-data.sh
+++ download-data.sh
@@ -1,4 +1,4 @@
 mkdir $(pwd)/data
 mkdir $(pwd)/data/raw
 
-kaggle datasets download -p $(pwd)/data/raw --unzip iafoss/siimacr-pneumothorax-segmentation-data-512
+kaggle datasets download -p $(pwd)/data/raw --unzip vbookshelf/pneumothorax-chest-xray-images-and-masks
