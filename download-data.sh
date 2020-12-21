0de70193d8d8d3776eaa4192472580513d7e3bfa
YazanShannak <yazansh7@hotmail.com>
2020-12-21 11:37:13 +0200

    Update download data from S3

--- download-data.sh
+++ download-data.sh
@@ -1,4 +1,7 @@
 mkdir $(pwd)/data
-mkdir $(pwd)/data/raw
+mkdir $(pwd)/data/processed
 
-kaggle datasets download -p $(pwd)/data/raw --unzip vbookshelf/pneumothorax-chest-xray-images-and-masks
+#kaggle datasets download -p $(pwd)/data/raw --unzip vbookshelf/pneumothorax-chest-xray-images-and-masks
+
+wget  https://yazansh-public.s3.amazonaws.com/mip-data.zip -P $(pwd)/data/processed
+unzip data/processed/mip-data.zip
