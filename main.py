10abb4bfa37edd430cbf0b5e5087beb03c5bd4a3
YazanShannak <yazansh7@hotmail.com>
2020-12-21 11:38:47 +0200

    Update autoencoder training script

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ processed_dir = os.path.abspath(os.path.join(data_dir, "processed"))
 images_datamodule = AutoencoderDataLoader(data_dir=processed_dir, batch_size=8)
 
 model_name = "autoencoder"
-version = 5
+version = 1
 lr = 1e-4
 
 checkpoint_callback = ModelCheckpoint(monitor="val_loss", mode="min")
 tensorboard_logger = TensorBoardLogger(save_dir="logs", name=model_name, version=version)
 lr_logger = LearningRateMonitor(logging_interval='epoch')
 
-model = AutoEncoder(lr=lr, gamma=0.5)
+model = AutoEncoder(lr=lr, gamma=0.75)
 
 trainer = pl.Trainer(gpus=-1, logger=tensorboard_logger, max_epochs=35, callbacks=[checkpoint_callback, lr_logger],
                      precision=16)
