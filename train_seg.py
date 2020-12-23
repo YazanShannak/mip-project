a34d625d369dc66ad09f1b08aef5a0f133630296
YazanShannak <yazansh7@hotmail.com>
2020-12-23 03:38:00 +0200

    Fix bug

--- train_seg.py
+++ train_seg.py
@@ -7,28 +7,27 @@ from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor, Ea
 
 data_dir = os.path.join(os.curdir, "data")
 processed_dir = os.path.abspath(os.path.join(data_dir, "processed"))
-checkpoint_path = os.path.join(os.curdir, "logs", "autoencoder", "version_10", "checkpoints", "epoch=6.ckpt")
+checkpoint_path = os.path.join(os.curdir, "logs", "autoencoder", "version_2", "checkpoints", "epoch=0.ckpt")
 images_datamodule = SegmentationLoader(data_dir=processed_dir, batch_size=16)
 
 model_name = "unet"
-version = 1
+version = "autoencoder-pretrained-1"
 lr = 1e-4
 gamma = 0.75
-freeze_encoder = False
+freeze_encoder = True
 
 checkpoint_callback = ModelCheckpoint(monitor="val_loss", mode="min")
 tensorboard_logger = TensorBoardLogger(save_dir="logs", name=model_name, version=version)
 lr_logger = LearningRateMonitor(logging_interval='epoch')
 early_stop = EarlyStopping(monitor="val_loss", min_delta=0.001, mode="min", patience=5)
 
-# model = SegmentationUnet.load_from_checkpoint(checkpoint_path=checkpoint_path, strict=True, lr=lr, gamma=gamma,
-#                                               freeze_encoder=freeze_encoder)
+model = SegmentationUnet.load_from_checkpoint(checkpoint_path=checkpoint_path, strict=True, lr=lr, gamma=gamma,
+                                              freeze_encoder=freeze_encoder)
 
-model = SegmentationUnet(lr=lr, gamma=gamma, freeze_encoder=False)
+# model = SegmentationUnet(lr=lr, gamma=gamma, freeze_encoder=False)
 
 trainer = pl.Trainer(gpus=-1, logger=tensorboard_logger, max_epochs=50,
-                     callbacks=[checkpoint_callback, lr_logger, early_stop],
-                     precision=16)
+                     callbacks=[checkpoint_callback, lr_logger, early_stop])
 
 if __name__ == "__main__":
     trainer = trainer.fit(model=model, datamodule=images_datamodule)
