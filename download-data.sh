74b8aa561f43725e2bf6ca59e503c5cfdf6aa91a
YazanShannak <yazansh7@hotmail.com>
2021-01-20 02:30:54 +0200

    Fix download data

--- download-data.sh
+++ download-data.sh
@@ -2,6 +2,6 @@ mkdir $(pwd)/data
 
 
 wget  https://yazansh-public.s3.amazonaws.com/mip-data.zip -P $(pwd)/data/
-unzip $(pwd)/data/processed/mip-data.zip -d $(pwd)/data
+unzip $(pwd)/data/mip-data.zip -d $(pwd)/data/
 
-rm $(pwd)/data/processed/mip-data.zip
+rm $(pwd)/data/mip-data.zip
